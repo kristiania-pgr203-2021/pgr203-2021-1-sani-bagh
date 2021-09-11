586ea6dee57b077bddc1d7f97997913a04825646
Saniya Baghirova <saniya.ibraimova@gmail.com>
2021-09-11 18:12:29 +0200

    Test shouldConvert6ToVI(passed)

--- Innlevering1/src/main/java/no/kristiania/ConvertToRoman.java
+++ Innlevering1/src/main/java/no/kristiania/ConvertToRoman.java
@@ -5,6 +5,14 @@ public class ConvertToRoman {
     public static String toRoman(int number) {
         String result = "";
 
+        while (number >= 5) {
+            result += "V";
+            number -= 5;
+        }
+        while (number >= 4) {
+            result += "IV";
+            number -= 4;
+        }
         while (number >= 1) {
             result += "I";
             number -= 1;
